411641ef8053c4ede8c860213b63d2a90ad8eb46
Oleksandr Tertyshnyi <alex_dimas@mail.ru>
2015-05-14 23:57:09 +0300

    Start studing git

--- TetsInClassroom/src/com/epam/lab/one/Main.java
+++ TetsInClassroom/src/com/epam/lab/one/Main.java
@@ -4,23 +4,10 @@ import java.util.ArrayList;
 
 public class Main {
 
-	public static final String EXAMPLE_TEST = "This is my small example "
-			+ "string which I'm going to " + "use for pattern matching.";
+	
 
 	public static void main(String[] args) {
 
-
-		System.out.println(EXAMPLE_TEST.matches("\\w.*"));
-
-		System.out.println(EXAMPLE_TEST.matches("\\w.*"));
-
-		String[] splitString = (EXAMPLE_TEST.split("\\s+"));
-		System.out.println(splitString.length);// should be 14
-		for (String string : splitString) {
-			System.out.println(string);
-		}
-		// replace all whitespace with tabs
-		System.out.println(EXAMPLE_TEST.replaceAll("\\s+", "\t"));
+			System.out.println("Studing git");
 	}
-
 }
